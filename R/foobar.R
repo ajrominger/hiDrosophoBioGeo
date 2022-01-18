9060d2861a1b19537afb86e6987d830ec1b6514b
ajrominger <ajrominger@gmail.com>
2022-01-18 15:43:15 -0500

    draft package working

---
+++ R/foobar.R
@@ -0,0 +1,15 @@
+#' @title Loads spatial data files
+#'
+#' @description testing
+#'
+#' @details stub
+#'
+#' @param x foo
+#'
+#' @return Stuff
+#'
+#' @export
+
+foobar <- function(x) {
+    x
+}
